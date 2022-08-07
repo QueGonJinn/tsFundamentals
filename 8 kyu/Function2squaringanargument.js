18b264f515801ccb98eed21dcf4081d2a7fb729c
QueGonJinn <molialow13@gmail.com>
2022-08-07 13:35:41 +0300

    feat: i decided Function 2 - squaring an argument

---
+++ 8 kyu/Function2squaringanargument.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.square = void 0;
+const square = (arg) => {
+    return arg ** 2;
+};
+exports.square = square;
+console.log((0, exports.square)(2));
---
+++ 8 kyu/Function2squaringanargument.ts
@@ -0,0 +1,5 @@
+export const square = (arg: number): number => {
+	return arg ** 2;
+}
+
+console.log(square(2));
